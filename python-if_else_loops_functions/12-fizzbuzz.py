24a89b3d47e0276d00fb31affa2da095bd07d3ee
Jehp00 <Jehp01104006@gmail.com>
2022-09-20 18:40:45 -0500

    add tow int, pow of tu int, FizzBuzz

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("{}".format("FizzBuzz"), end=" ")
+            i += 1
+        elif i % 3 == 0:
+            print("{}".format("Fizz"), end=" ")
+            i += 1
+        elif i % 5 == 0:
+            print("{}".format("Buzz"), end=" ")
+            i += 1
+        else:
+            print("{}".format(i), end=" ")
+            i += 1
