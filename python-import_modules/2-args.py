88659c3e2665343b0d5ac3cb791bb4bb936c6916
Jehp00 <Jehp01104006@gmail.com>
2022-09-22 15:58:46 -0500

    number of arguments

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -6,7 +6,7 @@ def _lenght():
     if len(argv) == 1:
         print("0 argument.")
     elif len(argv) == 2:
-        print("1 aguments:\n1: {}".format(argv[1]))
+        print("1 agument:\n1: {}".format(argv[1]))
     elif len(argv) > 2:
         print("{} arguments:".format(len(argv) - 1))
         for i in range(1, len(argv)):
