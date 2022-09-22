342eda52184401608c50118f6ffcd1172bc8cf2f
Jehp00 <Jehp01104006@gmail.com>
2022-09-22 15:30:15 -0500

    import jeje

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 from add_0 import add
-result = add(1, 2)
-print("1 + 2 = {}".format(result))
+def _add_():
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    _add_()
