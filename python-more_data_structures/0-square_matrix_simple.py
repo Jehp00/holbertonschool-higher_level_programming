7c32d223d78b31322f904a75aa608edd38cde76a
Jehp00 <Jehp01104006@gmail.com>
2022-09-26 23:45:33 -0500

    square_matrix_simple

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = [[0 for i in range(len(matrix[0]))] for i in range(len(matrix))]
+    for i in range(0, len(matrix)):
+        for j in range(0, len(matrix[0])):
+            new_matrix[i][j] = matrix[i][j] ** 2
+    return new_matrix
