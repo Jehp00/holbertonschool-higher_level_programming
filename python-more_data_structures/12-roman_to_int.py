93d65b5e8bafd58f81524a416553a3e815fb01db
Jehp00 <Jehp01104006@gmail.com>
2022-09-27 17:31:11 -0500

    roman to int

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -9,15 +9,15 @@ def roman_to_int(roman_string):
         'D': 500,
         'M': 1000}
     value = 0
-    num = 0
-    len_num = roman_string
+    n = 0
+    len_n = roman_string
     if type(roman_string) is not str:
         return 0
     if roman_string is None:
         return 0
-    for num in range(num, len(len_num)):
-        if num < len(len_num) - 1 and n_rom[len_num[num]] < n_rom[len_num[num + 1]]:
-            value -= n_rom[len_num[num]]
+    for n in range(n, len(len_n)):
+        if n < len(len_n) - 1 and n_rom[len_n[n]] < n_rom[len_n[n + 1]]:
+            value -= n_rom[len_n[n]]
         else:
-            value += n_rom[len_num[num]]
+            value += n_rom[len_n[n]]
     return value
