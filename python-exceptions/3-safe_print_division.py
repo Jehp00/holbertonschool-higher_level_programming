4d21b7a6ea5e1e58279090f0e0486b3242ed1987
Jehp00 <Jehp01104006@gmail.com>
2022-09-29 12:28:16 -0500

    print division with try

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        result = (a / b)
+    except BaseException:
+        return None
+    finally:
+        if b == 0:
+            print("Inside result: {}".format(None))
+        else:
+            print("Inside result: {}".format(a / b))
+    return result
