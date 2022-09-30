00d968c6813d99ba6dac948f24de35fb5accbf49
Jehp00 <Jehp01104006@gmail.com>
2022-09-30 15:17:24 -0500

    getter and setter

---
+++ python-classes/4-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Module square
+creating a class with one attribute Size
+"""
+
+
+class Square:
+    '''
+    class with attribute'''
+
+    def __init__(self, size=0):
+        '''attribute
+        arguments:
+            size: size of square'''
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+
+    @property
+    def size(self):
+        '''getter'''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        '''setter for size'''
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value
+
+
+    def area(self):
+        '''public method to calculate the area'''
+        area_square = self.__size ** 2
+        return area_square
