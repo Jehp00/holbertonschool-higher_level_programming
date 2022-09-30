c455ecbf0ddd4a14f94417c250e6602e0dd9e80f
Jehp00 <Jehp01104006@gmail.com>
2022-09-30 15:35:03 -0500

    #

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -14,12 +14,12 @@ class Square:
         arguments:
             size: size of square'''
         self.__size = size
-    
+
     @property
     def size(self):
         '''Return current size'''
         return self.__size
-    
+
     @size.setter
     def size(self, value):
         '''set the size'''
@@ -29,17 +29,17 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
-    
+
     def area(self):
         '''current square area'''
         return self.__size ** 2
-    
+
     def my_print(self):
         '''Prints to stdout the square with #'''
         if self.__size == 0:
             print()
         else:
-            for i in range (0, self.__size):
+            for i in range(0, self.__size):
                 for j in range(0, self.__size):
                     print("#", end="")
                 print()
