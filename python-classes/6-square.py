af8861d55bd7024e3ac13260f27f620083bc8487
Jehp00 <Jehp01104006@gmail.com>
2022-09-30 15:58:49 -0500

    # with a given position

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -55,7 +55,7 @@ class Square:
         if self.__size == 0:
             print()
             return
-        for l in range(0, self.__position[0]):
+        for l in range(0, self.__position[1]):
             print()
         for i in range(0, self.__size):
             for x in range(0, self.__position[0]):
