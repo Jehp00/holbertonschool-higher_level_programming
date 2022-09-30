4b652f4019289f62e1f205c0ff37f3305c5adbed
Jehp00 <Jehp01104006@gmail.com>
2022-09-30 11:05:38 -0500

    size of square

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module square
+creating a class with one attribute (size)
+"""
+
+
+class Square:
+    """class with attribute"""
+    def __init__(self, size):
+        '''attribute
+        args:
+            size: size of square'''
+        self.__size = size
