6df88b81d687069e10ccd9b6d0286319a5c6bf1b
Jehp00 <Jehp01104006@gmail.com>
2022-10-05 16:37:11 -0500

    square test

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""
+Module print_square
+Prints a square of # depending of the size
+"""
+
+
 def print_square(size):
+    '''
+    Prints a square where size is
+    the length and width of the square.'''
     if type(size) is not int:
         raise TypeError("size must be an integer")
     if size < 0:
