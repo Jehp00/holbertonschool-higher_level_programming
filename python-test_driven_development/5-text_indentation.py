d24d667cebcb74c09a37212ae5f43f85c19a7dff
Jehp00 <Jehp01104006@gmail.com>
2022-10-05 17:39:00 -0500

    PEP8

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -11,7 +11,7 @@ def text_indentation(text):
     """
     if type(text) is not str:
         raise TypeError("text must be a string")
-    
+
     for i in ".:?":
         text = (i + "\n\n").join(
             [line.strip(" ") for line in text.split(i)])
