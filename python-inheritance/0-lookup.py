9fd5335e6b64d8006f6b6009c935cf9fea0add93
Jehp00 <Jehp01104006@gmail.com>
2022-10-11 17:25:07 -0500

    README and lookup function for classes

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module lookup
+returns all the list of available
+attributes and methodsof an object
+"""
+def lookup(obj):
+    '''
+    returns the attributes and methods'''
+    return dir(obj)
