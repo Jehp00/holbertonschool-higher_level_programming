0f88373559148755815c9f411dc59efa7a06c654
Jehp00 <Jehp01104006@gmail.com>
2022-10-11 19:10:23 -0500

    sort list of ints

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
+"""
+Module MyList
+takes a unsorted list and sorted
+the numbers of the list
+"""
+
+
 class MyList(list):
+    '''
+    MyList class inheited from
+    list with nums unsirted'''
     def print_sorted(self):
+    '''
+    Public instance method that prints
+    the new list'''
         new_list = self[:]
         new_list.sort()
         print(new_list)
