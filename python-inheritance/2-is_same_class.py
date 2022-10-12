496fe6a788ec68f53e03b3c1e51564105940f017
Jehp00 <Jehp01104006@gmail.com>
2022-10-11 19:35:39 -0500

    is in class

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+"""
+Module is_same_class
+checks if an attribute is in a class
+"""
 def is_same_class(obj, a_class):
+    '''returns True or False if is not the same class
+    Args:
+        obj: attribute to compares
+        a_class: class to check
+    '''
     return isinstance(obj, a_class)
