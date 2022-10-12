9bf7b6f2e19b04920b9872c70bcfb237dee5d6e9
Jehp00 <Jehp01104006@gmail.com>
2022-10-11 19:48:03 -0500

    is in class or inherited class

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+"""
+Module is_kind_of_class
+checks if an attribute is in a class
+or inherited class
+"""
+
+
 def is_kind_of_class(obj, a_class):
+    '''return True if is in class, False otherwise'''
     return isinstance(obj, a_class)
