33dfb6f028d1027851bcfd9ca799b2476e079879
Jehp00 <Jehp01104006@gmail.com>
2022-10-13 16:04:24 -0500

    inherits class

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,5 +5,8 @@ checks if obj is in a specified class
 directly or indirectly
 """
 def inherits_from(obj, a_class):
-    '''Returns True if is in class, False otherwise'''
-    return issubclass(obj, a_class)
+    '''Returns True if is in class, False otherwise
+    args:
+        obj: object to look
+        a_class: class to check'''
+    return isinstance(obj, a_class) and type(obj) != a_class
