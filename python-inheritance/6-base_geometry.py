1500a2651a1999d12b3ce45d3bfa5c2c76c78b6e
Jehp00 <Jehp01104006@gmail.com>
2022-10-13 16:15:51 -0500

    Exception geometry class

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """
 Module BaseGeometry
-empty class
+cretes a methood area but without
+any parameter
 """
 
 
 class BaseGeometry:
-    """Any attribute for this class"""
+    """BaseGeometry class that give a messege error 
+    where the method arrea is not implemented"""
     def area(self):
+        '''messege error'''
         raise Exception("area() is not implemented")
