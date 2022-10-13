b98eee6d46f009b2505ea9078d963d02e6b32234
Jehp00 <Jehp01104006@gmail.com>
2022-10-13 16:38:00 -0500

    integer validator with test

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Module BaseGeometry
+cretes a methood area but without
+any parameter and have an integer validator
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry class that give a messege error
+    where the method arrea is not implemented
+    and checks for the value of integers"""
+    def area(self):
+        '''messege error by area()'''
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        '''validator of the value of datas if are int
+        args:
+            name: string/variable to be check messege
+            value: data to be check if is int'''
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
