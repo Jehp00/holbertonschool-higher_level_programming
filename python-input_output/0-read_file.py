f2aa377cba202a70a0ff203d735c71c9482ac54b
Jehp00 <Jehp01104006@gmail.com>
2022-10-13 19:46:32 -0500

    read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -9,5 +9,5 @@ def read_file(filename=""):
     '''read the file and print it
     args:
         filename: string or text to be read'''
-    with open('filename', encoding="utf-8") as file:
+    with open('my_file_0', encoding="utf-8") as file:
         read_data = f.read()
