ef94cbae9195e37b3fc176ca776684e213328712
Jehp00 <Jehp01104006@gmail.com>
2022-10-19 15:32:13 -0500

    Rectangle messege

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -99,5 +99,5 @@ class Rectangle(Base):
 
     def __str__(self):
         '''method so that it returns [Rectangle] (<id>) <x>/<y> - <width>/<height>'''
-        return ("[Rectangle] ({}) {}/{} {}/{}".format(self.id, self.__x, self.__y,
+        return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y,
                 self.__width, self.__height))
