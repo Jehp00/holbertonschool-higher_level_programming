4badeb384e47ba6c805e91f510f6e9b72919b9ff
Jehp00 <Jehp01104006@gmail.com>
2022-10-19 09:21:42 -0500

    pakage python and base class

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module base
+creates a Base class for the pakage python
+"""
+
+
+class Base:
+    """Class with private attribute and init method
+    with the public ittribute id"""
+    __nb_objects = 0
+    def __init__(self, id=None):
+        '''Method that assigne a value to the arg id and count the number of obj
+        args:
+            id: id to asigne value'''
+        if id is not None:
+            self.id = id
+        else:
+            self.__nb_objects += 1
+            self.id = id
