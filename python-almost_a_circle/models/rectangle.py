5ef76460bbf31444a86bef21e19710b3c16e89f5
Jehp00 <Jehp01104006@gmail.com>
2022-10-19 12:01:27 -0500

    rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
 """
-
+Module rectangle
 """
+
 
 from models.base import Base
 #Base = __import__('models/base').Base
 
 
 class Rectangle(Base):
+    '''Rectangle class inherited from Base class'''
     def __init__(self, width, height, x=0, y=0, id=None):
+        '''initialization asiigns the attributres of a rectangle
+        args:
+            width: width of the rectangle
+            height: height of rectangle
+            x: position in x
+            y: position in y'''
         super().__init__(id)
         self.__width = width
         self.__height = height
@@ -18,35 +26,43 @@ class Rectangle(Base):
     # width
     @property
     def width(self):
+        '''getter'''
         return self.__width
 
     @width.setter
     def width(self, value):
-        pass
+        '''setter'''
+        self.__width = value
 
     # height
     @property
     def height(self):
+        '''getter'''
         return self.__height
 
     @width.setter
     def height(self, value):
-        pass
+        '''setter'''
+        self.__height = value
 
     # x
     @property
     def x(self):
+        '''gettter'''
         return self.__x
 
     @x.setter
     def width(self, value):
-        pass
+        '''setter'''
+        self.__x = value
 
     # y
     @property
     def y(self):
+        '''getter'''
         return self.__y
 
     @y.setter
     def y(self, value):
-        pass
+        '''setter'''
+        self.__y = value
