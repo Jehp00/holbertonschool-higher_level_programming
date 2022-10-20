3af1a633189547613c570cc794af83441ba42e85
Jehp00 <Jehp01104006@gmail.com>
2022-10-20 16:26:54 -0500

    Square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -10,10 +10,26 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """
+    Square class inherited from Rectangule with the same methods
+    """
     def __init__(self, size, x=0, y=0, id=None):
+        '''
+        initialization of Square base rectangle
+        args:
+            id: id of square
+            size: width and height of square
+            x: position of square in eje x
+            y: position of square in eje y
+        '''
         self.size = size
         super().__init__(size, size, x, y, id)
     def __str__(self):
+        '''
+        return [Square] (<id>) <x>/<y> - <size>
+        size in our case is width public
+        instance from Rectangle
+        '''
         s = "[Square] ({}) {}/{} - {}".format(
             self.id, self.x, self.y, self.width)
         return s
