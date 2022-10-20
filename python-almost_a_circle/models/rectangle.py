8d25fd4c8594a5456aa6a4e2e3542eb0aee0e1c6
Jehp00 <Jehp01104006@gmail.com>
2022-10-20 11:40:22 -0500

    Updating attr with  args

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -105,3 +105,31 @@ class Rectangle(Base):
         (<id>) <x>/<y> - <width>/<height>'''
         return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x,
                 self.__y, self.__width, self.__height))
+
+
+    def update(self, *args):
+        '''updating attributes with args'''
+        if len(args) == 5:
+            self.id = args[0]
+            self.__width = args[1]
+            self.__height = args[2]
+            self.__x = args[3]
+            self.__y = args[4]
+
+        if len(args) == 4:
+            self.id = args[0]
+            self.__width = args[1]
+            self.__height = args[2]
+            self.__x = args[3]
+
+        if len(args) == 3:
+            self.id = args[0]
+            self.__width = args[1]
+            self.__height = args[2]
+
+        if len(args) == 2:
+            self.id = args[0]
+            self.__width = args[1]
+
+        if len(args) == 1:
+            self.id = args[0]
