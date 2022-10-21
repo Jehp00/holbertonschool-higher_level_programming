f33ccd5a770364e15b35ba2863f868722a6d9dd2
Jehp00 <Jehp01104006@gmail.com>
2022-10-20 22:40:02 -0500

    JSON list of dictionaries

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -5,6 +5,9 @@ creates a Base class for the pakage python
 """
 
 
+import json
+
+
 class Base:
     """Class with private attribute and init method
     with the public ittribute id"""
@@ -20,8 +23,15 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
-        '''Retunrs a list od dictionaries to JSON strings'''
-        if list_dictionaries is None or list_dictionaries == 0:
-            return []
-        return(list(list_dictionaries))
+        '''Retunrs a list od dictionaries to JSON strings
+        Args:
+            list_objs: list of instances who inherits of Base
+        '''
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        if type(list_dictionaries) != list or\
+            not all(type(x) == dict for x in list_dictionaries):
+            raise TypeError("list_dictionaries must be a list of dictionaries")
+        return json.dumps(list_dictionaries)
