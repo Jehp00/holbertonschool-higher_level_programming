c73fb06b92c033eb14eb241032ed506a52692c8b
Jehp00 <Jehp01104006@gmail.com>
2022-10-20 23:00:45 -0500

    class method list JSON str to file

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -35,3 +35,20 @@ class Base:
            not all(type(x) == dict for x in list_dictionaries)):
             raise TypeError("list_dictionaries must be a list of dictionaries")
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        '''
+        writes the JSON string representation
+        of list_objs to a file
+        args:
+            cls: class objet
+            list_objs: list of instances who inherits of Base
+        '''
+        if list_objs is None or list_objs == []:
+            Jsstr = "[]"
+        else:
+            Jsstr = cls.to_json_string([0.to_dictionary() for o in list_objs])
+        filename = cls.__name__ + '.json'
+        with open(filename, 'w') as f:
+            f.write(Jsstr)
