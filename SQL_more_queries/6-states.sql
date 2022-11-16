115a592322b4d5b3986ffc42d2ccdd05cdfeae42
Jehp00 <Jehp01104006@gmail.com>
2022-11-16 09:37:49 -0500

    creates a databse and a tabkle insaide the database

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,5 +1,5 @@
 -- creates the database hbtn_0d_usa and the table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
-    id INT NOT NULL UNIQUE AUTO_INCREMET PRIMARY KEY,
-    name VARCHAR(256) NOT NULL);
+    'id' INT NOT NULL UNIQUE AUTO_INCREMET PRIMARY KEY,
+    'name' VARCHAR(256) NOT NULL);
