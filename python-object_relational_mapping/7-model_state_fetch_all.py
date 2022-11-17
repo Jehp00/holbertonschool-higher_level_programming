c6aa4a53e38a8b1c7c74ed1ec5c2a913d14c62ca
Jehp00 <Jehp01104006@gmail.com>
2022-11-17 15:27:47 -0500

    model state fetch all

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """cript that lists all State objects
    from the database hbtn_0e_6_usa"""
-import sys
+from sys import argv
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import (create_engine)
