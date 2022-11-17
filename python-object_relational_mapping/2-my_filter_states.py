4d41e6e6d275b26dcf38266e40033cb6d1a10ff3
Jehp00 <Jehp01104006@gmail.com>
2022-11-17 11:45:16 -0500

    filter states like argument

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+takes in an argument and displays all values in the
+states table of hbtn_0e_0_usa where name matches the argument.
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    '''take the connection to the server'''
+    connection = MySQLdb.connect(host="localhost",
+                                 port=3306,
+                                 user=sys.argv[1],
+                                 passwd=sys.argv[2],
+                                 db=sys.argv[3],
+                                 state_name_searched=sys.argv[4]
+                                 charset='utf8')
+    '''start the cursor'''
+    cursor = connection.cursor()
+
+    '''execute the query'''
+    cursor.execute("SELECT * FROM states WHERE name={} ORDER BY id ASC".format(state_name_searched))
+    rows = cursor.fetchall()
+
+    '''print the rows/queries'''
+    for r in rows:
+        print(r)
+
+    '''Close the cursor'''
+    cursor.close()
+    connection.close()
