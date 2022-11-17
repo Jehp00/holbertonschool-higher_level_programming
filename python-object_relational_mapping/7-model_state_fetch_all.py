ee35f2a07dbe47bf8263c2b39275e47d053c213d
Jehp00 <Jehp01104006@gmail.com>
2022-11-17 15:25:40 -0500

    model state fetch all

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 """cript that lists all State objects
    from the database hbtn_0e_6_usa"""
-
-
 import sys
 from model_state import Base, State
-from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
 
 
 def model_state():
     """initializate function model_state for db"""
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format\
-    (sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1],
+        argv[2],
+        argv[3]),
+        pool_pre_ping=True)
     '''start engine'''
     Base.metadata.create_all(engine)
     '''start session'''
