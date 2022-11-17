f2ea6676bc6d29bc6194597d174bd0ea1bd2eabe
Jehp00 <Jehp01104006@gmail.com>
2022-11-17 18:54:26 -0500

    model city fetch its city

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -13,6 +13,6 @@ class City(Base):
     Args:
         Base class inherited'''
     __tablename__ = "cities"
-    id = Column('id', Integer, autoincrement=auto, nullable=False, primary_key=True, unique=True)
-    name = Column('name', String(128), nullable=False)
-    state_id = Column('state_id', Integer, nullable=False, Foreignkey=('states.id'))
+    id = Column(Integer, autoincrement=auto, nullable=False, primary_key=True, unique=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, nullable=False, Foreignkey=('states.id'))
