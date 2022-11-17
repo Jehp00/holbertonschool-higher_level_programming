51852f9dc34b24e89f67186a61ddffc842db43a7
Jehp00 <Jehp01104006@gmail.com>
2022-11-17 13:16:17 -0500

    filter the states but safe

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+takes in arguments and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument.
+But this time, write one that is safe from MySQL injections!
+"""
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    '''take the connection to the server'''
+    connection = MySQLdb.connect(host="localhost",
+                                 port=3306,
+                                 user=sys.argv[1],
+                                 passwd=sys.argv[2],
+                                 db=sys.argv[3],
+                                 charset='utf8')
+    '''start the cursor'''
+    cursor = connection.cursor()
+
+    '''execute the query'''
+    cursor.execute("SELECT * FROM states WHERE \
+                    name=%s"(sys.argv[4]), )
+    rows = cursor.fetchall()
+
+    '''print the rows/queries'''
+    for r in rows:
+        print(r)
+
+    '''Close the cursor'''
+    cursor.close()
+    connection.close()
