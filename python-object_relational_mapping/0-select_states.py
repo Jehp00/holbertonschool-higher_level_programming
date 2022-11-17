7bbefa5c784a472f397e1549b97e6e1d9c68bfcd
Jehp00 <Jehp01104006@gmail.com>
2022-11-17 10:49:34 -0500

    lists the states since python

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -10,9 +10,9 @@ if __name__ == "__main__":
     #take the connection to the server
     connection = MySQLdb.connect(host="localhost",
                                 port=3306,
-                                user="sudo",
+                                user=sys.argv[1],
                                 passwd=sys.argv[2],
-                                database=sys.argv[3],
+                                db=sys.argv[3],
                                 charset='utf8')
     #start the cursor
     cursor = conncection.cursor()
