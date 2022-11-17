31598694f647a43c4105820e53594aef9a360d25
Jehp00 <Jehp01104006@gmail.com>
2022-11-17 10:52:27 -0500

    lists the states since python

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,24 +7,24 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    #take the connection to the server
+    '''take the connection to the server'''
     connection = MySQLdb.connect(host="localhost",
                                 port=3306,
                                 user=sys.argv[1],
                                 passwd=sys.argv[2],
                                 db=sys.argv[3],
                                 charset='utf8')
-    #start the cursor
+    '''start the cursor'''
     cursor = connection.cursor()
 
-    #execute the query
+    '''execute the query'''
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
     rows = cursor.fetchall()
 
-    #print the rows/queries
+    '''print the rows/queries'''
     for r in rows:
         print(r)
 
-    #Close the cursor
+    '''Close the cursor'''
     cursor.close()
     connection.close()
