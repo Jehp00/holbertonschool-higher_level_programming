05d31038c046440d3da2a1485fe7804b574e3c78
Jehp00 <Jehp01104006@gmail.com>
2022-11-17 17:21:59 -0500

    model state filtering the letter a

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""prints the first State object from the database hbtn_0e_6_usa"""
+"""lists all State objects that contain the letter
+a from the database hbtn_0e_6_usa"""
 from sqlalchemy import create_engine
 from model_state import Base, State
 import sys
