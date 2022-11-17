4d2960b6a1304cf1c9001804b2c26aa5f7606dda
Jehp00 <Jehp01104006@gmail.com>
2022-11-17 14:51:16 -0500

    model state class

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -12,7 +12,7 @@ class State(Base):
     """inherits from Base
     Args:
         Base class inherited"""
-    __table__ = 'states'
+    __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False,
-                unique=True, auto_increment=True)
+                unique=True, autoincrement=True)
     name = Column(String(128), nullable=False)
