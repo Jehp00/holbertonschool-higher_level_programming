8c726be4b3e85b47b6dbfd1136938fad109809d6
Jehp00 <Jehp01104006@gmail.com>
2022-11-17 16:50:34 -0500

    first record of table

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""prints the first State object from the database hbtn_0e_6_usa"""
+from sqlalchemy import create_engine
+from model_state import Base, State
+import sys
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1],
+        sys.argv[2],
+        sys.argv[3]),
+        pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    '''associate it with our custom Session class'''
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    Query = session.query(State).filter_by(State.name=sys.argv[5])
+
+    if Query is not None:
+        print(f"{Query.id}")
+
+    session.close()
