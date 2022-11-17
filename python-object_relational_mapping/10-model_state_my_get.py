f04080e9d465e0e4adcd755f5238bf89237302d6
Jehp00 <Jehp01104006@gmail.com>
2022-11-17 17:34:45 -0500

    model state my get id

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""lists all State objects that contain the letter
-a from the database hbtn_0e_6_usa"""
+"""prints the State object with the name passed as
+argument from the database hbtn_0e_6_usa"""
 from sqlalchemy import create_engine
 from model_state import Base, State
 import sys
@@ -21,7 +21,7 @@ if __name__ == '__main__':
 
     session = Session()
 
-    Query = session.query(State).filter(State.name = sys.argv[4]).first()
+    Query = session.query(State).filter(State.name == sys.argv[4]).first()
 
     if Query:
         print(Query.id)
