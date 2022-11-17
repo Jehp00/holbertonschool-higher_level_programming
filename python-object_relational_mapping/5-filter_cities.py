88f489103bd7b8031ac38c6bc38177d591fccddf
Jehp00 <Jehp01104006@gmail.com>
2022-11-17 14:35:31 -0500

    filter cities depending of the arg

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
 
     '''execute the query'''
     cursor.execute("SELECT cities.name FROM cities\
-        JOIN states ON cities.state.id=states.id WHERE \
+        JOIN states ON cities.state_id=states.id WHERE \
         states.name=%s ORDER BY cities.id ASC", (sys.argv[4], ))
 
     rows = cursor.fetchall()
