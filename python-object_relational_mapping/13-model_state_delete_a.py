bfc0038715ec8572e1fb8c40cedb688263ec7e3c
Jehp00 <Jehp01104006@gmail.com>
2022-11-18 21:00:17 -0500

    deletes the state with a specifique name

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,29 +1,24 @@
 #!/usr/bin/python3
 """deletes all State objects with a name containing the letter
 a from the database hbtn_0e_6_usa"""
-from sqlalchemy import create_engine
+
+
 from model_state import Base, State
 import sys
+from sqlalchemy import (create_engine, update)
 from sqlalchemy.orm import sessionmaker
 
 
-if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1],
-        sys.argv[2],
-        sys.argv[3]),
-        pool_pre_ping=True)
-
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-    '''associate it with our custom Session class'''
     Session = sessionmaker(bind=engine)
-    session = Session()
-
-    Query = session.query(State).filter(State.name.like('%a%'))
-
-    for Instances in Query:
-        session.delete(Instances)
-
-    session.commit()
-    session.close()
+    Session = Session()
+    Query = Session.query(State).filter(State.name.like('%a%'))
+    for Instance in Query:
+        Session.delete(Instance)
+    Session.commit()
+    Session.close()
