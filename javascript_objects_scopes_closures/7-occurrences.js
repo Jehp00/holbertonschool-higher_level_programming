7c477d9b77c4ed1cea2425db655993666539a164
Jehp00 <Jehp01104006@gmail.com>
2022-12-20 15:45:07 -0500

    task 7

---
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  for (const number of list) {
+    if (number === searchElement) {
+      count++;
+    }
+  }
+  return count;
+};
