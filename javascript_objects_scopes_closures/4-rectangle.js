e4768a3d33d7c49693d747815659a1f6a07babff
Jehp00 <Jehp01104006@gmail.com>
2022-12-20 13:51:46 -0500

    task 4

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const char = 'X';
+    for (let i = 0; i <= this.height - 1; i++) {
+      let Rect = '';
+      for (let j = 0; j <= this.width - 1; j++) {
+        Rect += char;
+      }
+      console.log(Rect);
+    }
+  }
+
+  rotate () {
+    const newWidth = this.width;
+    const newHeight = this.height;
+    this.height = newWidth;
+    this.width = newHeight;
+    return (this.height, this.width);
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+    return (this.height, this.width);
+  }
+};
