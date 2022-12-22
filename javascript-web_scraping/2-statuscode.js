78df33397a0fe66f9d752c4b6b82cf6a2f0b6d92
Jehp00 <Jehp01104006@gmail.com>
2022-12-21 19:50:58 -0500

    task 2

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const args = process.argv.slice(2)
+const args = process.argv.slice(2);
 const link = args[0];
 const request = require('request');
 request(link, (error, response, body) => {
-    if (error) {
-        console.error('error:', error);
-    }
-    console.log('code:', response && response.statusCode);
+  if (error) {
+    console.error('error:', error);
+  }
+  console.log('code:', response && response.statusCode);
 });
